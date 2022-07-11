e0f30ed306b448984f73423917a69c762fc14a4f
Firuz Gulomamadov <firuz.gulomamadov@gmail.com>
2022-07-11 19:09:28 -0400

    class 2,3

---
+++ Modules/Class3/stringsConcept.js
@@ -0,0 +1,13 @@
+/**
+ * To compare if two Strings are equal
+ * function: localCompare
+ * 
+ * if both String are equal, function return 0
+ * 
+ */
+
+let word = 'laUgH';
+
+let toLowerCase = word.toLowerCase();
+let toUperCase = toLowerCase.replace(toLowerCase.toUpperCase(toLowerCase.charAt(0)));
+console.log(toUperCase);
